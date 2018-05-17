f31bbd3c6e9672e7f65f6e3f02071e6d22f5843c
NaveenShroff <naveenkumarrips@gmail.com>
2018-05-17 16:55:54 +0530

    Update 10_python_challenge.py

--- Python/10_python_challenge.py
+++ Python/10_python_challenge.py
@@ -1,5 +1,5 @@
 #Challenge
-#Find the numbers whıch can dive 4 and 4 multiple(tetragenous) in from 0 to 100
+#Find the numbers whıch can divide 4 and 4 multiple(tetragenous) in from 0 to 100
 for n in range(101):
     if(n % 4 is 0):
         print(n)
